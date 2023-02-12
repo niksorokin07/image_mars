293c2ee811d1e7cb979ccd6b036f53059aff2166
niksorokin07 <117514209+niksorokin07@users.noreply.github.com>
2023-02-12 13:58:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,149 @@
+from flask import Flask, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def printt():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return """<h1>Человечество вырастает из детства.</h1>
+            <h1>Человечеству мала одна планета.</h1>
+            <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
+            <h1>И начнем с Марса!</h1>
+            <h1>Присоединяйся!</h1>"""
+
+
+@app.route('/promotion_image')
+def image_mars():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                  <link rel="stylesheet" 
+                    href="{url_for('static', filename='style/style.css')}">
+                    <h1>Жди нас, Марс!</h1>
+                    <img src={url_for('static', filename='img/mars.png')} alt="no img">
+                    <div class="alert alert-dark" role="alert">
+                        Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                        Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                <link rel="stylesheet"
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                crossorigin="anonymous">
+                                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='style/style2.css')}" />
+                                <title>Пример формы</title>
+                              </head>
+                              <body>
+                                <h1>Форма для регистрации в суперсекретной системе</h1>
+                                <div>
+                                    <form class="login_form" method="post">
+                                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите фамилию" name="email">
+                                        <input type="password" class="form-control" id="password" placeholder="Введите имя" name="password">
+                                        <input type="password" class="form-control" id="password" placeholder="Введите адрес почты" name="password">
+                                        <div class="form-group">
+                                            <label for="classSelect">Какое у вас обраование?</label>
+                                            <select class="form-control" id="classSelect" name="class">
+                                              <option>нет</option>
+                                              <option>среднее</option>
+                                              <option>высшее</option>
+                                            </select>
+                                         </div>
+                                        <div class="form-group">
+                                            <label for="about">почему вы хотите полететь?</label>
+                                            <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="photo">Приложите фотографию</label>
+                                            <input type="file" class="form-control-file" id="photo" name="file">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="form-check">Укажите пол</label>
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                              <label class="form-check-label" for="male">
+                                                Мужской
+                                              </label>
+                                            </div>
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                              <label class="form-check-label" for="female">
+                                                Женский
+                                              </label>
+                                            </div>
+                                        </div>
+                                         <label for="form-group form-check">Укажите работу</label>
+                                        <div class="form-group form-check">
+                                            <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                            <label class="form-check-label" for="acceptRules">врач</label>
+                                        </div>
+                                         <div class="form-group form-check">
+                                            <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                            <label class="form-check-label" for="acceptRules">инженер-исследователь</label>
+                                        </div>
+                                          <div class="form-group">
+                                            <label for="photo">Приложите фотографию</label>
+                                            <input type="file" class="form-control-file" id="photo" name="file">
+                                        </div>
+                                         <div class="туц">
+                                            <input type="checkbox" class="ready?" id="acceptRules" name="accept">
+                                            <label class="form-check-label" for="acceptRules">готовы ли вы остаться на марсе?</label>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Записаться</button>
+                                    </form>
+                                </div>
+                              </body>
+                            </html>'''
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
